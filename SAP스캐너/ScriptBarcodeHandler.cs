8bd263c3611065591e1f4a2126656e2a6f656f18
kackac000 <kackac000@gmail.com>
2026-03-07 20:31:32 +0900

    SAP스캐너 소스코드 업로드

---
+++ SAP스캐너/ScriptBarcodeHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAP스캐너
+{
+    internal class ScriptBarcodeHandler
+    {
+        public bool TryProcessBarcode(IntPtr targetHwnd, string barcode)
+        {
+            // TODO: 나중에 스크립트 자동화 실제 코드 구현
+            return false; // 지금은 항상 실패(즉시 키마 분기로 빠짐)
+        }
+    }
+}
